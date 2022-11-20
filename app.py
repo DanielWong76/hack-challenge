e0adf0469c7eb6e951de374d550908322e7b96db
Daniel Wong <djw329@cornell.edu>
2022-11-20 02:20:41 -0500

    merging

--- app.py
+++ app.py
@@ -2,39 +2,39 @@ import os
 from ast import Assign
 from multiprocessing.util import ForkAwareThreadLock
 from unittest.mock import NonCallableMagicMock
-import db
+from db import db, Profile, Asset, Job, Rating
 from flask import Flask, request
 import json
 import users_dao
 import datetime
-
-
-
+ 
+ 
+ 
 app = Flask(__name__)
-db_filename = "cms.db"
-
+db_filename = "hack.db"
+ 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-
+ 
 db.init_app(app)
 with app.app_context():
     db.create_all()
-
-
+ 
+ 
 def success_response(data, code=200):
     """
     Returns a generic success response
     """
     return json.dumps(data), code
-
-
+ 
+ 
 def failure_response(message, code=404):
     """
     Returns a generic failure response
     """
     return json.dumps({"error": message}), code
-
+ 
 def extract_token(request):
     """
     Helper function that extracts the token from the header of a request
@@ -42,36 +42,454 @@ def extract_token(request):
     auth_header = request.headers.get("Authorization")
     if auth_header is None:
         return False, failure_response("Missing Authorization header.", 400)
-
+ 
     #Header looks like Authorization: Bearer fkafpkakfpow
     bearer_token = auth_header.replace("Bearer ", "").strip()
     if bearer_token is None or not bearer_token:
         return False, failure_response("Invalid authorization header", 400)
-
+ 
     return True, bearer_token
-
+ 
 @app.route("/")
 def hello_world():
     """
     Endpoint for printing Hello World!
     """
     return "Hello World!"
-
-
-@app.route("/register/", methods=["POST"])
+ 
+ 
+@app.route("/api/register/", methods=["POST"])
 def register_account():
     """
     Endpoint for registering a new user
     """
-    return "Hello"
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+ 
+    if email is None or password is None:
+        return failure_response("Missing email or password", 400)
+ 
+    success, user = users_dao.create_user(email, password)
+    if not success:
+        return failure_response("User already exists", 400)
+   
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
+    )
+ 
+ 
+ 
+@app.route("/api/login/", methods=["POST"])
+def login():
+    """
+    Endpoint for logging in a user
+    """
+    body = json.loads(request.data)
+    email = body.get("email")
+    password = body.get("password")
+ 
+    if email is None or password is None:
+        return failure_response("Missing password or email", 400)
+ 
+    success, user = users_dao.verify_credentials(email, password)
+ 
+    if not success:
+        return failure_response("Incorrect email or password", 401)
+ 
+    return success_response({
+        "session_token": user.session_token,
+        "session_expiration": str(user.session_expiration),
+        "update_token": user.update_token
+    })
+ 
+ 
+@app.route("/api/session/", methods=["POST"])
+def update_session():
+    """
+    Endpoint for updating a user's session
+    """
+    success, update_token = extract_token(request)
+    success_user, user = users_dao.renew_session(update_token)
+ 
+    if not success_user:
+        return failure_response("Invalid update token", 400)
+   
+    return success_response(
+        {
+            "session_token": user.session_token,
+            "session_expiration": str(user.session_expiration),
+            "update_token": user.update_token
+        }
+    )
+ 
+ 
+@app.route("/api/secret/", methods=["GET"])
+def secret_message():
+    """
+    Endpoint for verifying a session token and returning a secret message
+ 
+    In your project, you will use the same logic for any endpoint that needs
+    authentication
+    """
+    success, session_token = extract_token(request)
+    if not success:
+        return failure_response("Could not extract session token", 400)
+   
+    user = users_dao.get_user_by_session_token(session_token)
+    if user is None or not user.verify_session_token(session_token):
+        return failure_response("Session token Invalid", 400)
+ 
+    return success_response({"message": "You have successfully implemented sessions!"})
+ 
+ 
+@app.route("/api/logout/", methods=["POST"])
+def logout():
+    """
+    Endpoint for logging out a user
+    """
+    success, session_token = extract_token(request)
+ 
+    if not success:
+        return failure_response("Could not extract session token", 400)
+ 
+    user = users_dao.get_user_by_session_token(session_token)
+    if user is None or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token", 400)
+ 
+    user.session_token = ""
+    user.session_expiration = datetime.datetime.now()
+    user.update_token = ""
+ 
+    return success_response({"message": "You have successfully logged out"})
 
+@app.route("/api/profile/")
+def get_profiles():
+    """
+    Endpoint for the getting all Profiles
+    """
+    profile = [profile.serialize() for profile in Profile.query.all()]
+    return success_response({"profile": profile})
 
+@app.route("/api/profile/", methods = ["POST"])
+def create_profile():
+    """
+    Endpoint for creating a profile
+    """
+    body = json.loads(request.data)
+    first = body.get("first")
+    last = body.get("last")
+    email = body.get("email")
+    phone_number = body.get("phone_number")
+    if first is None or last is None or email is None or phone_number is None:
+        return failure_response("Missing first name, last name, email, or phone number", 400)
+    profile = Profile(first = first, last = last, email = email, phone_number = phone_number)
+    db.session.add(profile)
+    db.session.commit()
+    return success_response(profile.serialize(), 201)
 
+@app.route("/api/profile/<int:profile_id>/", methods = ["POST"])
+def update_profile(profile_id):
+    """
+    Endpoint for updating a profile
+    """
+    profile = Profile.query.filter_by(id = profile_id).first()
+    body = json.loads(request.data)
+    first = body.get("first")
+    last = body.get("last")
+    email = body.get("email")
+    phone_number = body.get("phone_number")
+    if first is None or last is None or email is None or phone_number is None:
+        return failure_response("Missing first name, last name, email, or phone number", 400)
+    profile.first = first 
+    profile.last = last 
+    profile.email = email 
+    profile.phone_number = phone_number 
+    db.session.commit()
+    return success_response(profile.serialize(), 201)
 
+@app.route("/api/profile/<int:profile_id>/")
+def get_profile(profile_id):
+    """
+    Endpoint for getting a profile by id
+    """
+    profile = Profile.query.filter_by(id = profile_id).first()
+    if profile is None:
+        return failure_response("Profile not found!")
+    return success_response(profile.serialize())
 
+@app.route("/api/profile/<int:profile_id>/", methods=["DELETE"])
+def delete_profile(profile_id):
+    """
+    Endpoint for deleting a profile by id
+    """
+    profile = Profile.query.filter_by(id = profile_id).first()
+    if profile is None:
+        return failure_response("Profile not found!")
+    db.session.delete(profile)
+    db.session.commit()
+    return success_response(profile.serialize())
+    
 
+#-----------------IMAGES--------------------------------------------
 
+@app.route("/api/upload/", methods=["POST"])
+def upload():
+    """
+    Endpoint for uploading an image to AWS given its base64 form,
+    then storing/returning the URL of that image
+    """
+    body = json.loads(request.data)
+    image_data = body.get("image_data")
+    if image_data is None:
+        return failure_response("No base64 image found")
+    
+    asset = Asset(image_data = image_data)
+    db.session.add(asset)
+    db.session.commit()
+    return success_response(asset.serialize(), 201)
 
+#-----------------JOBS--------------------------------------------
 
+@app.route("/api/job/")
+def get_jobs():
+    """
+    Endpoint for getting all jobs
+    """
+    jobs = [job.serialize() for job in Job.query.all()]
+    return success_response({"jobs": jobs})
+
+@app.route("/api/job/", methods=["POST"])
+def create_job():
+    """
+    Endpoint for creating a job
+    """
+    body = json.loads(request.data)
+    title = body.get("title")
+    description = body.get("description")
+    location = body.get("location")
+    date_activity = body.get("date_activity")
+    duration = body.get("duration")
+    reward = body.get("reward")
+    if title is None or description is None or  location is None or  date_activity is None or duration is None or reward is None:
+        return failure_response("Missing one of the required fields", 400)
+    job = Job(title = title, description = description, location = location, date_activity =date_activity, duration=duration, reward=reward)
+    db.session.add(job)
+    db.session.commit()
+    return success_response(job.serialize(), 201)
+
+@app.route("/api/job/<int:job_id>/", methods = ["POST"])
+def update_job(job_id):
+    """
+    Endpoint for updating a job
+    """
+    job = Job.query.filter_by(id = job_id).first()
+    body = json.loads(request.data)
+    title = body.get("title")
+    description = body.get("description")
+    location = body.get("location")
+    date_activity = body.get("date_activity")
+    duration = body.get("duration")
+    reward = body.get("reward")
+    if title is None or description is None or  location is None or  date_activity is None or duration is None or reward is None:
+        return failure_response("Missing one of the required fields", 400)
+    job.title = title 
+    job.description = description 
+    job.location = location 
+    job.date_activity = date_activity 
+    job.duration = duration 
+    job.reward = reward 
+    db.session.commit()
+    return success_response(job.serialize(), 201)
+
+@app.route("/api/job/<int:job_id>/")
+def get_job(job_id):
+    """
+    Endpoint for getting a job by id
+    """
+    job = Job.query.filter_by(id = job_id).first()
+    if job is None:
+        return failure_response("Job not found!")
+    return success_response(job.serialize())
+
+@app.route("/api/job/<int:job_id>/", methods=["DELETE"])
+def delete_job(job_id):
+    """
+    Endpoint for deleting a job by id
+    """
+    job = Job.query.filter_by(id = job_id).first()
+    if job is None:
+        return failure_response("Job not found!")
+    db.session.delete(job)
+    db.session.commit()
+    return success_response(job.serialize())@app.route("/api/profile/")
+
+def get_profiles():
+    """
+    Endpoint for the getting all Profiles
+    """
+    profile = [profile.serialize() for profile in Profile.query.all()]
+    return success_response({"profile": profile})
+
+@app.route("/api/profile/", methods = ["POST"])
+def create_profile():
+    """
+    Endpoint for creating a profile
+    """
+    body = json.loads(request.data)
+    first = body.get("first")
+    last = body.get("last")
+    email = body.get("email")
+    phone_number = body.get("phone_number")
+    if first is None or last is None or email is None or phone_number is None:
+        return failure_response("Missing first name, last name, email, or phone number", 400)
+    profile = Profile(first = first, last = last, email = email, phone_number = phone_number)
+    db.session.add(profile)
+    db.session.commit()
+    return success_response(profile.serialize(), 201)
+
+@app.route("/api/profile/<int:profile_id>/", methods = ["POST"])
+def update_profile(profile_id):
+    """
+    Endpoint for updating a profile
+    """
+    profile = Profile.query.filter_by(id = profile_id).first()
+    body = json.loads(request.data)
+    first = body.get("first")
+    last = body.get("last")
+    email = body.get("email")
+    phone_number = body.get("phone_number")
+    if first is None or last is None or email is None or phone_number is None:
+        return failure_response("Missing first name, last name, email, or phone number", 400)
+    profile.first = first 
+    profile.last = last 
+    profile.email = email 
+    profile.phone_number = phone_number 
+    db.session.commit()
+    return success_response(profile.serialize(), 201)
+
+@app.route("/api/profile/<int:profile_id>/")
+def get_profile(profile_id):
+    """
+    Endpoint for getting a profile by id
+    """
+    profile = Profile.query.filter_by(id = profile_id).first()
+    if profile is None:
+        return failure_response("Profile not found!")
+    return success_response(profile.serialize())
+
+@app.route("/api/profile/<int:profile_id>/", methods=["DELETE"])
+def delete_profile(profile_id):
+    """
+    Endpoint for deleting a profile by id
+    """
+    profile = Profile.query.filter_by(id = profile_id).first()
+    if profile is None:
+        return failure_response("Profile not found!")
+    db.session.delete(profile)
+    db.session.commit()
+    return success_response(profile.serialize())
+    
+
+#-----------------IMAGES--------------------------------------------
+
+@app.route("/api/upload/", methods=["POST"])
+def upload():
+    """
+    Endpoint for uploading an image to AWS given its base64 form,
+    then storing/returning the URL of that image
+    """
+    body = json.loads(request.data)
+    image_data = body.get("image_data")
+    if image_data is None:
+        return failure_response("No base64 image found")
+    
+    asset = Asset(image_data = image_data)
+    db.session.add(asset)
+    db.session.commit()
+    return success_response(asset.serialize(), 201)
+
+#-----------------JOBS--------------------------------------------
+
+@app.route("/api/job/")
+def get_jobs():
+    """
+    Endpoint for getting all jobs
+    """
+    jobs = [job.serialize() for job in Job.query.all()]
+    return success_response({"jobs": jobs})
+
+@app.route("/api/job/", methods=["POST"])
+def create_job():
+    """
+    Endpoint for creating a job
+    """
+    body = json.loads(request.data)
+    title = body.get("title")
+    description = body.get("description")
+    location = body.get("location")
+    date_activity = body.get("date_activity")
+    duration = body.get("duration")
+    reward = body.get("reward")
+    if title is None or description is None or  location is None or  date_activity is None or duration is None or reward is None:
+        return failure_response("Missing one of the required fields", 400)
+    job = Job(title = title, description = description, location = location, date_activity =date_activity, duration=duration, reward=reward)
+    db.session.add(job)
+    db.session.commit()
+    return success_response(job.serialize(), 201)
+
+@app.route("/api/job/<int:job_id>/", methods = ["POST"])
+def update_job(job_id):
+    """
+    Endpoint for updating a job
+    """
+    job = Job.query.filter_by(id = job_id).first()
+    body = json.loads(request.data)
+    title = body.get("title")
+    description = body.get("description")
+    location = body.get("location")
+    date_activity = body.get("date_activity")
+    duration = body.get("duration")
+    reward = body.get("reward")
+    if title is None or description is None or  location is None or  date_activity is None or duration is None or reward is None:
+        return failure_response("Missing one of the required fields", 400)
+    job.title = title 
+    job.description = description 
+    job.location = location 
+    job.date_activity = date_activity 
+    job.duration = duration 
+    job.reward = reward 
+    db.session.commit()
+    return success_response(job.serialize(), 201)
+
+@app.route("/api/job/<int:job_id>/")
+def get_job(job_id):
+    """
+    Endpoint for getting a job by id
+    """
+    job = Job.query.filter_by(id = job_id).first()
+    if job is None:
+        return failure_response("Job not found!")
+    return success_response(job.serialize())
+
+@app.route("/api/job/<int:job_id>/", methods=["DELETE"])
+def delete_job(job_id):
+    """
+    Endpoint for deleting a job by id
+    """
+    job = Job.query.filter_by(id = job_id).first()
+    if job is None:
+        return failure_response("Job not found!")
+    db.session.delete(job)
+    db.session.commit()
+    return success_response(job.serialize())
+ 
+ 
+ 
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- db.py
+++ db.py
@@ -17,6 +17,248 @@ import string
 db = SQLAlchemy()
 
 
+class Profile(db.Model):
+    """
+    Profile Model
+    """
+    __tablename__ = "profile"
+    id =  db.Column(db.Integer, primary_key = True, autoincrement = True)
+    first = db.Column(db.String, nullable = False)
+    last = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, nullable = False)
+    phone_number = db.Column(db.Integer, nullable = False)
+    #user_id 
+    #image_id
+    #chat
+    #job_id
+    #rating
+
+    def __init__(self, **kwargs):
+        """
+        Creates a Profile Object
+        """
+        self.first = kwargs.get("first")
+        self.last = kwargs.get("last")
+        self.email = kwargs.get("email")
+        self.phone_number = kwargs.get("phone_number")
+
+    def serialize(self):
+        """
+        Serializes an Profile object
+        """
+        return {
+            "id" : self.id,
+            "first" : self.first,
+            "last" : self.last,
+            "email" : self.email,
+            "phone_number" : self.phone_number,
+        }
+    def simple_serialize(self):
+        """
+        Serializes an Profile object without any other class
+        """
+        return {
+            "id" : self.id,
+            "first" : self.first,
+            "last" : self.last,
+            "email" : self.email,
+            "phone_number" : self.phone_number,
+        }
+
+#-----------------IMAGES--------------------------------------------
+
+EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
+BASE_DIR = os.getcwd()
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-2.amazonaws.com"
+
+class Asset(db.Model):
+    """
+    Asset Model
+    """
+    __tablename__ = "asset"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    base_url = db.Column(db.String, nullable = False)
+    salt = db.Column(db.String, nullable = False)
+    extension = db.Column(db.String, nullable = False)
+    width = db.Column(db.Integer, nullable = False)
+    height = db.Column(db.Integer, nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False)
+
+    def __init__(self, **kwargs):
+        """
+        Initializes an asset object
+        """
+        self.create(kwargs.get("image_data"))
+    
+    def create(self, image_data):
+        """
+        Given an image in base64 encoding, does the following
+        1. Rejects the image if it is not a supported filetype
+        2. Generate a random string for the image filename
+        3. Decodes the image and attempts to upload to AWS
+        """ 
+        try:
+            ext = guess_extension(guess_type(image_data)[0])[1:]
+            if ext not in EXTENSIONS:
+                raise Exception(f"Extension {ext} is not valid!")
+            
+            salt = "".join( #random generator creates filename
+                random.SystemRandom().choice(
+                    string.ascii_uppercase + string.digits
+                )
+                for _ in range(16)
+            )
+            
+            img_str = re.sub("^data:image/.+;base64,", "", image_data)
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+            self.created_at = datetime.now()
+
+            img_filename = f"{self.salt}.{self.extension}"
+            self.upload(img, img_filename)
+        except Exception as e:
+            print(f"Error when creating image: {e}")
+    
+    def upload(self, img, img_filename):
+        """
+        Attempts to upload the image into the specified S3 bucket
+        """
+        try:
+            #save image into temporary
+            img_temp_loc = f"{BASE_DIR}/{img_filename}"
+            img.save(img_temp_loc)
+
+            #upload image into S3 bucket
+            s3_client = boto3.client("s3")
+            s3_client.upload_file(img_temp_loc, S3_BUCKET_NAME, img_filename)
+            
+            s3_resource = boto3.resource("s3")
+            object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME, img_filename) 
+            object_acl.put(ACL = "public-read")
+
+            #remove img from temp location
+            os.remove(img_temp_loc)
+        except Exception as e:
+            print(f"Error when uploading image: {e}")
+        
+    def serialize(self):
+        """
+        Serializes an asset object
+        """
+        return {
+            "url": f"{self.base_url}/{self.salt}.{self.extension}",
+            "created_at": str(self.created_at)
+        }
+    
+#-----------------JOBS--------------------------------------------
+
+class Job(db.Model):
+    """
+    Job Model
+    """
+    __tablename__ = "job"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = False)
+    location = db.Column(db.String, nullable = False)
+    date_created = db.Column(db.DateTime, nullable = False)
+    date_activity = db.Column(db.String, nullable = False)
+    duration = db.Column(db.Integer, nullable = False)
+    reward = db.Column(db.String, nullable = False)
+    done = db.Column(db.Boolean, nullable = False)
+    taken = db.Column(db.Boolean, nullable = False)
+    #poster_id
+    #receiver_id
+    #img_id
+    #potential_id
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a job object
+        """
+        self.title = kwargs.get("title")
+        self.description = kwargs.get("description")
+        self.location = kwargs.get("location")
+        self.date_created = datetime.now()
+        self.date_activity = kwargs.get("date_activity")
+        self.duration = kwargs.get("duration")
+        self.reward = kwargs.get("reward")
+        self.done = False
+        self.taken = False
+    
+    def serialize(self):
+        """
+        Serializes a job object
+        """
+        return{
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "location": self.location,
+            "date_created": str(self.date_created),
+            "date_activity": self.date_activity,
+            "duration": self.duration,
+            "reward": self.reward,
+            "done": self.done,
+            "taken": self.taken
+        }
+
+    def simple_serialize(self):
+        """
+        Serializes a job object
+        """
+        return{
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "location": self.location,
+            "date_created": str(self.date_created),
+            "date_activity": self.date_activity,
+            "duration": self.duration,
+            "reward": self.reward,
+            "done": self.done,
+            "taken": self.taken
+        }
+
+#-----------------RATINGS--------------------------------------------
+    
+class Rating(db.Model):
+    """
+    Rating Model
+    """
+    __tablename__ = "rating"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True )
+    rate = db.Column(db.Integer, nullable = False)
+    description = db.Column(db.String, nullable = False)
+    #poster_id
+    #postee_id
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a rating object
+        """
+        self.rate = kwargs.get("rate")
+        self.description = kwargs.get("description")
+    
+    def serialize(self):
+        return{
+            "rate": self.rate,
+            "description": self.description
+        }
+    
+    def simple_serialize(self):
+        return{
+            "rate": self.rate,
+            "description": self.description
+        }
+
 class User(db.Model):
     """
     User model
@@ -75,45 +317,3 @@ class User(db.Model):
         Verifies the update token of a user
         """
         return update_token == self.update_token
-
-class Rating(db.Model):
-    """
-    Rating Model
-    """
-    __tablename__ = "rating"
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True )
-    rate = db.Column(db.Integer, nullable = False)
-    description = db.Column(db.String, nullable = False)
-    #poster_id
-    #postee_id
-
-    def __init__(self, **kwargs):
-        """
-        Initializes a rating object
-        """
-        self.rate = kwargs.get("rate")
-        self.description = kwargs.get("description")
-    
-    def serialize(self):
-        return{
-            "rate": self.rate,
-            "description": self.description
-        }
-    
-    def simple_serialize(self):
-        return{
-            "rate": self.rate,
-            "description": self.description
-        }
-
-    
-
-
-
-
-    
-
-        
-
-
-
