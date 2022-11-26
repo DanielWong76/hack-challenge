b78a98c96207cadd0045ff55ac2de97b49b7d02a
DanielWong76 <danielwong0505@gmail.com>
2022-11-26 15:16:02 -0500

    changed register account route to include user info

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ import datetime
  
 app = Flask(__name__)
 db_filename = "hack.db"
-
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
  
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -80,14 +77,12 @@ def register_account():
     success, user = users_dao.create_user( email, password, first, last, phone_number)
     if not success:
         return failure_response("User already exists", 400)
-   
-    return success_response(
-        {
-            "session_token": user.session_token,
-            "session_expiration": str(user.session_expiration),
-            "update_token": user.update_token
-        }, 201
-    )
+    user_serialize = user.serialize()
+    user_serialize["session_token"] = user.session_token
+    user_serialize["session_expiration"] = str(user.session_expiration)
+    user_serialize["update_token"] = user.update_token
+
+    return success_response(user_serialize , 201)
  
  
  
